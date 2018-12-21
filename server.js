f8695b951ec75259140eba162a3ec6606fdea64e
chandulella23 <chandu.sai@venturepact.com>
2018-12-21 20:41:52 +0530

    done

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const app = require('./server/init');
 const http = require('http');
 const server = http.createServer(app);
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
-server.listen(port);
-console.log("server started ......")
+server.listen(port,(err)=>{
+    if(!err)
+    console.log("server started ......")
+});
+
